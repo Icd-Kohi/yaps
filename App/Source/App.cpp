23b47024afef255300d8f610df72742003398256
Icd-Kohi <luambo13@gmail.com>
2026-04-28 22:40:22 -0300

    first

---
+++ App/Source/App.cpp
@@ -0,0 +1,24 @@
+#include "../../Core/Source/Core/Core.h"
+#include <cstdint>
+#include <sys/types.h>
+
+#define SIZE(obj) sizeof(obj)/sizeof(obj[0])
+
+typedef u_int16_t u16;
+typedef u_int32_t u32;
+typedef int16_t i16;
+typedef int32_t i32;
+
+
+int Core::main(){
+    int thing = 0;
+    return thing;
+};
+
+int main()
+{
+	//Core::Printing();
+        
+    return Core::main();
+}
+
---
+++ Core/Source/Core/Core.cpp
@@ -0,0 +1,13 @@
+#include "Core.h"
+
+#include <iostream>
+
+namespace Core {
+
+	void Printintg()
+	{
+		std::cout << "Make the file whenever I want.!\n";
+		std::cin.get();
+	}
+
+}
